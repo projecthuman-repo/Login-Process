932e2a633d29f19bea3ceb3c7be6ba24537d1a9f
Shivam Aery <shivamaeryy08@gmail.com>
2023-02-15 06:12:36 -0500

    Add validators for login

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -3,41 +3,57 @@ const bcrypt = require("bcrypt");
 const loginRouter = require("express").Router();
 const User = require("../models/user");
 
-loginRouter.post("/", async (request, response) => {
-  const { username, password } = request.body;
+loginRouter.post(
+  "/",
+  body("username")
+    .not()
+    .isEmpty()
+    .trim()
+    .escape()
+    .withMessage("Invalid input for username"),
+  body("password")
+    .not()
+    .isEmpty()
+    .trim()
+    .escape()
+    .withMessage("Invalid input for password"),
 
-  const user = await User.findOne({ username });
-  const passwordCorrect =
-    user === null ? false : await bcrypt.compare(password, user.passwordHash);
+  async (request, response) => {
+    const { username, password } = request.body;
 
-  if (!(user && passwordCorrect)) {
-    return response.status(401).json({
-      status: "Fail",
-      error: "Invalid username or password",
+    const user = await User.findOne({ username });
+    const passwordCorrect =
+      user === null ? false : await bcrypt.compare(password, user.passwordHash);
+
+    if (!(user && passwordCorrect)) {
+      return response.status(401).json({
+        status: "Fail",
+        error: "Invalid username or password",
+      });
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user._id,
+    };
+
+    // token expires in 60 * 60 seconds (1hr)
+    const token = jwt.sign(
+      userForToken,
+      process.env.SECRET,
+      /*     { expiresIn: "60 * 60" } */
+      { expiresIn: "20m" } //Change to 20 minutes, research and found it to be recommended
+    );
+
+    response.status(200).json({
+      status: "Success",
+      data: {
+        token,
+        username: user.username,
+        firstName: user.firstName,
+      },
     });
   }
-
-  const userForToken = {
-    username: user.username,
-    id: user._id,
-  };
-
-  // token expires in 60 * 60 seconds (1hr)
-  const token = jwt.sign(
-    userForToken,
-    process.env.SECRET,
-    /*     { expiresIn: "60 * 60" } */
-    { expiresIn: "20m" } //Change to 20 minutes, research and found it to be recommended
-  );
-
-  response.status(200).json({
-    status: "Success",
-    data: {
-      token,
-      username: user.username,
-      firstName: user.firstName,
-    },
-  });
-});
+);
 
 module.exports = loginRouter;
