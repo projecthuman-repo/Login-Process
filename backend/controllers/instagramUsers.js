50c6c3384bfa8f9a8db95333d9834f7bc692d825
Shivam Aery <shivamaeryy08@gmail.com>
2023-03-20 05:20:26 -0400

    Remove unneeded error validation and check if fb/insta user already exists

--- backend/controllers/instagramUsers.js
+++ backend/controllers/instagramUsers.js
@@ -11,61 +11,28 @@ instagramUsersRouter.get("/", async (request, response) => {
   });
 });
 
-instagramUsersRouter.post(
-  "/",
-  body("firstName")
-    .not()
-    .isEmpty()
-    .trim()
-    .escape()
-    .withMessage("Invalid input for first name"),
-  body("lastName")
-    .not()
-    .isEmpty()
-    .trim()
-    .escape()
-    .withMessage("Invalid input for last name"),
-  body("phoneNumber")
-    .not()
-    .isEmpty()
-    .trim()
-    .escape()
-    .isMobilePhone()
-    .withMessage("Invalid input for phone number"),
-  body("email")
-    .isEmail()
-    .normalizeEmail()
-    .withMessage("Email entered is not a valid email"),
-    body("username")
-    .not()
-    .isEmpty()
-    .trim()
-    .escape()
-    .withMessage("Invalid input for username"),
-  async (request, response) => {
-    const errors = validationResult(request);
-    if (!errors.isEmpty()) {
-      return response.status(401).json({
-        status: "Fail",
-        errors: errors.array(),
-      });
-    }
+instagramUsersRouter.post("/", async (request, response) => {
+  const { firstName, lastName, email, phoneNumber } = request.body;
 
-    const { firstName, lastName, email, phoneNumber } = request.body;
-
-    const instagramUser = new InstagramUser({
-      firstName,
-      lastName,
-      email,
-      phoneNumber,
-    });
-
-    const savedInstagramUser = await instagramUser.save();
-    response.status(201).json({
+  if (InstagramUser.find({ email: email }) !== null) {
+    return response.status(200).json({
       status: "Success",
-      savedInstagramUser,
+      message: "User successfully logged in",
     });
   }
-);
+
+  const instagramUser = new InstagramUser({
+    firstName,
+    lastName,
+    email,
+    phoneNumber,
+  });
+
+  const savedInstagramUser = await instagramUser.save();
+  response.status(201).json({
+    status: "Success",
+    savedInstagramUser,
+  });
+});
 
 module.exports = instagramUsersRouter;
