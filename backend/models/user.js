a99333ac4cb560d0819ccdaaedb7a6fc0f16a62c
starrlight90 <swayam.pendgaonkar@gmail.com>
2023-06-08 12:10:33 +0200

    Individual Model schemas addition

--- backend/models/user.js
+++ backend/models/user.js
@@ -15,6 +15,38 @@ const mongoose = require("mongoose");
 const crypto = require("crypto");
 const uniqueValidator = require("mongoose-unique-validator");
 
+// Define the UserRank schema
+const userRankSchema = new mongoose.Schema({
+  userRankID: { type: String, required: true, unique: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  rank: { type: mongoose.Schema.Types.ObjectId, ref: 'Rank', required: true },
+  app: { type: mongoose.Schema.Types.ObjectId, ref: 'App', required: true },
+  dateAchieved: { type: Date, required: true },
+  isActive: { type: Boolean, default: false },
+});
+
+// Define the UserActivity schema
+const userActivitySchema = new mongoose.Schema({
+  userActivityID: { type: String, required: true, unique: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  activity: { type: mongoose.Schema.Types.ObjectId, ref: 'Activity', required: true },
+  datePerformed: { type: Date, required: true },
+  pointsEarned: { type: Number, required: true },
+});
+
+// Define the UserApp schema
+const userAppSchema = new mongoose.Schema({
+  // UserApp schema fields and their types
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  app: { type: mongoose.Schema.Types.ObjectId, ref: 'App', required: true },
+  appVersion: { type: mongoose.Schema.Types.ObjectId, ref: 'App Version', required: true },
+  lastActivityDate: { type: Date },
+  totalActivityTime: { type: Number },
+  appRank: { type: mongoose.Schema.Types.ObjectId, ref: 'Rank' },
+});
+
+
+
 /**
  * @constructor user
  */
@@ -58,28 +90,17 @@ const userSchema = new mongoose.Schema({
     default: false,
   },
 
-  userRank: {
-    type: String,
-    required: [true, "A user must have ranking"],
-    unique: false,
-  },
-
-  userActivity:{
-    type: [String],
-    required: [false],
-    unique: false,
-
-  },
-
-  appName:{
-    type: [String],
-    required : [false],
-    unique: false,
-  },
-
   passwordResetToken: String,
   passwordResetExpires: Date,
   passwordChangedAt: Date,
+
+ // Embed UserRank subdocument
+ rank: { type: userRankSchema, required: true },
+ // Embed UserActivity subdocument
+ activity: { type: userActivitySchema, required: true },
+ // Embed UserApp subdocument
+ apps: [userAppSchema],
+
 });
 // Add better validations for uniqueness
 userSchema.plugin(uniqueValidator);
