7160d8d6d161a1e24e2c2c3b221ce2aa02c3ffb4
christian-phc <christian.dev@projecthumancity.com>
2024-05-30 11:18:58 -0400

    Removed bootstrap in favor of custom CSS files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // REACT_APP_GOOGLE_CLIENT_ID is the id used for googleoauth to work for google sign in
